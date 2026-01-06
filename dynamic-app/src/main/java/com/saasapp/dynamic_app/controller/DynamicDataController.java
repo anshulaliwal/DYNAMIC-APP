d5af4b3c3916c7424cbe8b86252d0e2c7c0fd6a2
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2026-01-06 17:28:19 +0530

    cors origins added for ui

--- dynamic-app/src/main/java/com/saasapp/dynamic_app/controller/DynamicDataController.java
+++ dynamic-app/src/main/java/com/saasapp/dynamic_app/controller/DynamicDataController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/dynamic")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"},
+@CrossOrigin(origins = {"https://client-ca-saas-app.vercel.app", "https://super-saas-app.vercel.app", "http://localhost:3000", "http://localhost:3001"},
              allowedHeaders = "*",
              methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS},
              allowCredentials = "true",
